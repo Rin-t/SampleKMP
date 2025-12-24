ba9be7f2cb3db18f2aa36be8f842b2ba701232dc
Rin-t <rin.t.4921@gmail.com>
2025-12-24 22:52:45 +0900

    update

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    kotlin("plugin.serialization") version "2.3.0"
-    id("co.touchlab.skie") version "0.10.8"
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.skie)
 }
 
 kotlin {
@@ -18,22 +18,21 @@ kotlin {
             isStatic = true
         }
     }
-    val ktorVersion = "3.3.3"
     sourceSets {
         commonMain.dependencies {
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-            implementation("io.ktor:ktor-client-core:$ktorVersion")
-            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
         }
         androidMain.dependencies {
-            implementation("io.ktor:ktor-client-android:$ktorVersion")
-            implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
-            implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
-            implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+            implementation(libs.ktor.client.android)
+            implementation(libs.androidx.lifecycle.viewmodel.ktx)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.androidx.lifecycle.viewmodel.compose)
         }
         iosMain.dependencies {
-            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            implementation(libs.ktor.client.darwin)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
